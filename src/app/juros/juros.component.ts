505d11ba6255a138f892c4e4490fc9896c7bb130
juliadelua <juliagatoh@gmail.com>
2022-03-18 20:17:59 -0300

    Initial commit

---
+++ src/app/juros/juros.component.ts
@@ -0,0 +1,29 @@
+import { newArray } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juros',
+  templateUrl: './juros.component.html',
+  styleUrls: ['./juros.component.css'],
+})
+export class JurosComponent implements OnInit {
+  vp: number;
+  j: number;
+  n: number;
+
+  constructor() {
+    this.vp = 100;
+    this.j = 2.5;
+    this.n = 3;
+  }
+
+  ngOnInit() {}
+
+  getJuros() {
+    return this.vp * (1 + (this.j / 100) * this.n);
+  }
+
+  getJurosComp(meses: number) {
+    return this.vp * Math.pow(1 + this.j / 100, meses);
+  }
+}
